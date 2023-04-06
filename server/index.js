2a45de2a8885f752f2194ec25312d5e80f5fde1f
Jouni Juntunen <jouni.juntunen@oamk.fi>
2023-04-06 10:13:59 +0300

    Added file upload to the backend.

--- server/index.js
+++ server/index.js
@@ -6,8 +6,12 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const cors_1 = __importDefault(require("cors"));
 const pg_1 = require("pg");
+const express_fileupload_1 = __importDefault(require("express-fileupload"));
 const app = (0, express_1.default)();
 app.use((0, cors_1.default)());
+app.use(express_1.default.json());
+app.use(express_1.default.urlencoded({ extended: true }));
+app.use((0, express_fileupload_1.default)());
 app.use(express_1.default.static("public"));
 const port = 3003;
 app.get("/", (req, res) => {
@@ -20,6 +24,25 @@ app.get("/", (req, res) => {
         }
         res.status(200).json(result.rows);
     });
+});
+app.post("/upload", (req, res) => {
+    if (!req.files) {
+        res.statusMessage = "Image was not received";
+        res.sendStatus(400);
+        return;
+    }
+    const file = req.files.image;
+    const name = file.name;
+    //const uploadPath = './public/images/' + name
+    const uploadPath = `./public/images/${name}`;
+    file.mv(uploadPath, (err) => {
+        if (err) {
+            res.statusMessage = err;
+            res.sendStatus(500);
+            return;
+        }
+    });
+    res.sendStatus(200);
 });
 app.listen(port);
 const openDb = () => {
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,14 @@
 import express, { Express, Request,Response } from 'express'
 import cors from 'cors'
 import { Pool,QueryResult } from 'pg'
+import fileUpload, { UploadedFile } from 'express-fileupload'
 
 const app: Express = express()
 
 app.use(cors())
+app.use(express.json())
+app.use(express.urlencoded({extended: true}))
+app.use(fileUpload())
 app.use(express.static("public"))
 
 const port = 3003
@@ -19,6 +23,30 @@ app.get("/",(req: Request,res: Response) => {
     }
     res.status(200).json(result.rows)
   })
+})
+
+
+app.post("/upload",(req: Request,res: Response) => {
+  if (!req.files) {
+    res.statusMessage= "Image was not received"
+    res.sendStatus(400)
+    return
+  }
+
+  const file = req.files.image as UploadedFile
+  const name = file.name
+  //const uploadPath = './public/images/' + name
+  const uploadPath = `./public/images/${name}`
+
+  file.mv(uploadPath,(err) => {
+    if (err) {
+      res.statusMessage = err
+      res.sendStatus(500)
+      return
+    }
+  })
+
+  res.sendStatus(200)  
 })
 
 app.listen(port)
